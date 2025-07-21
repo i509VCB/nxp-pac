be4dd0936c20d5897364a381b1d95a99514c1e7e
i509VCB <git@i509.me>
2025-07-21 17:48:30 -0500

    imxrt: export 1062 metadata

--- src/metadata.rs
+++ src/metadata.rs
@@ -1,4 +1,5 @@
 #[cfg_attr(feature = "mimxrt1011", path = "./chips/mimxrt1011/metadata.rs")]
+#[cfg_attr(feature = "mimxrt1062", path = "./chips/mimxrt1062/metadata.rs")]
 mod _generated;
 
 pub use _generated::*;
