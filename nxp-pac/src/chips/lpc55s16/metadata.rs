c54814bbb514495a7a5aebad7d17b987b7b70c0f
Corentin AZAIS <c.azais@ikalogic.com>
2026-07-30 13:07:01 +0200

    metadata: add USB0 peripheral for LPC55

--- nxp-pac/src/chips/lpc55s16/metadata.rs
+++ nxp-pac/src/chips/lpc55s16/metadata.rs
@@ -2040,6 +2040,15 @@ pub const PERIPHERALS: &[Peripheral] = &[
         dma_muxing: &[],
         gate: None,
     },
+    Peripheral {
+        name: "USB0",
+        address: 0x4008_4000,
+        driver_name: "",
+        signals: &[],
+        flexcomm: None,
+        dma_muxing: &[],
+        gate: None,
+    },
 ];
 pub const INTERRUPTS: &[(&str, u32)] = &[
     ("WDT_BOD", 0u32),
